0407ce83db309f6223f81036b707794208d6597a
iamovsyanka <anakstkv@gmail.com>
2020-11-21 11:10:06 +0300

    feat: add system(pause)

--- TCP/ServerT/ServerT/ServerT.cpp
+++ TCP/ServerT/ServerT/ServerT.cpp
@@ -3,68 +3,57 @@
 #include <ctime>
 
 #include "ErrorMsgText.h"
-#include "Winsock2.h"                // заголовок  WS2_32.dll
-#pragma comment(lib, "WS2_32.lib")   // экспорт  WS2_32.dll
+#include "Winsock2.h"                
+#pragma comment(lib, "WS2_32.lib")   
 
 int main()
 {
     setlocale(LC_ALL, "rus");
 
-    WSADATA wsaData;      //запись служебной информации
-
-    SOCKET  sS;           // дескриптор сокета 
-    SOCKADDR_IN serv;                    // параметры  сокета sS
-    serv.sin_family = AF_INET;           // используется IP-адресация  
-    serv.sin_port = htons(2000);         // порт 2000
-    serv.sin_addr.s_addr = INADDR_ANY;   // любой собственный IP-адрес 
+    WSADATA wsaData;      
+    SOCKET  sS;           
+    SOCKADDR_IN serv;                    
+    serv.sin_family = AF_INET;            
+    serv.sin_port = htons(2000);        
+    serv.sin_addr.s_addr = INADDR_ANY;  
 
     try {
         cout << "ServerT\n\n";
+
         if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
-            //версия. Младший байт основная версия, старший байт расширение версии.
-            //параметры инициализации
             throw  SetErrorMsgText("Startup: ", WSAGetLastError());
         }
         if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET) {
-            //функция  позволяет создать сокет (точнее дескриптор сокета) и задать его характеристики 
-            //SOCK_STREEM – сокет ориентированный на поток;    
             throw  SetErrorMsgText("socket: ", WSAGetLastError());
         }
         if (bind(sS, (LPSOCKADDR)&serv, sizeof(serv)) == SOCKET_ERROR) {
-            // связать сокет с параметрами
             throw  SetErrorMsgText("bind: ", WSAGetLastError());
         }
         if (listen(sS, SOMAXCONN) == SOCKET_ERROR) {
-            // переключить сокет в режим прослушивания
             throw SetErrorMsgText("listen: ", WSAGetLastError());
         }
 
-        SOCKET cS;	                            // сокет для обмена данными с клиентом 
-        SOCKADDR_IN clnt;                       // параметры  сокета клиента
-        memset(&clnt, 0, sizeof(clnt));         // обнулить память
-        int lclnt = sizeof(clnt);               // размер SOCKADDR_IN
+        SOCKET cS;	                            
+        SOCKADDR_IN clnt;                       
+        memset(&clnt, 0, sizeof(clnt));         
+        int lclnt = sizeof(clnt);               
 
         clock_t start, end;
-        char ibuf[50],                          //буфер ввода 
-             obuf[50] = "server: принято ";     //буфер вывода
-        int  libuf = 0,                         //количество принятых байт
-             lobuf = 0;                         //количество отправленных байт 
+        char ibuf[50], obuf[50] = "server: принято ";     
+        int  libuf = 0, lobuf = 0;                         
 
         if ((cS = accept(sS, (sockaddr*)&clnt, &lclnt)) == INVALID_SOCKET) {
-            // разрешить подключение к сокету
             throw SetErrorMsgText("accept: ", WSAGetLastError());
         }
 
         start = clock();
         while (true) {
             if ((libuf = recv(cS, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR) {
-                //функция принимает заданное  количество байт данных по каналу определенного сокета  
                 throw SetErrorMsgText("recv: ", WSAGetLastError());
             }
 
             string obuf = ibuf;
             if ((lobuf = send(cS, obuf.c_str(), strlen(obuf.c_str()) + 1, NULL)) == SOCKET_ERROR) {
-                //функция пересылает заданное  количество байт данных по каналу определенного сокета  
                 throw SetErrorMsgText("send: ", WSAGetLastError());
             }
 
@@ -75,15 +64,12 @@ int main()
         cout << "Time for send and recv: " << ((double)(end - start) / CLK_TCK) << " c" << endl;
 
         if (closesocket(cS) == SOCKET_ERROR) {
-            //переводит сокет в неработоспособное состояние и освобождает все ресурсы связанные с ним  
             throw  SetErrorMsgText("closesocket: ", WSAGetLastError());
         }
-        if (closesocket(sS) == SOCKET_ERROR) {
-            //переводит сокет в неработоспособное состояние и освобождает все ресурсы связанные с ним  
+        if (closesocket(sS) == SOCKET_ERROR) { 
             throw  SetErrorMsgText("closesocket: ", WSAGetLastError());
         }
         if (WSACleanup() == SOCKET_ERROR) {
-            // завершить  работу с библиотекой  WS2_32.DLL
             throw  SetErrorMsgText("Cleanup: ", WSAGetLastError());
         }
     }
@@ -91,5 +77,6 @@ int main()
         cout << endl << errorMsgText;
     }
 
+    system("pause");
     return 0;
 }
